5008d23c7b5db1c1f25e3bd986833ea46d760954
rahma408 <61933946+rahma408@users.noreply.github.com>
2020-10-06 22:54:09 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace currency_apps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CurrencyController currency;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currency = new CurrencyController();
+        }
+        private void button_hitung_click(object sender, RoutedEventArgs e)
+        {
+            var nominalinput = input_textBox.Text;
+            var result = "invalid";
+            if (currency.isAllowed(nominalinput))
+            {
+                result = currency.usdToIdr(nominalinput);
+            }
+            resultlabel.Content = result;
+        }
+    }
+}
